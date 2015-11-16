995829a540df27c86ff18929e01ab69be1b952d4
Alex Jackson <alex@alexj.org>
2015-11-15 18:33:01 -0700

    Add basic mutate function to GA

--- mt1dga.jl
+++ mt1dga.jl
@@ -7,6 +7,7 @@ global nLayers  = 3
 global maxDepth = 100_000
 global resRange = 1:100
 global data     = zeros(1,5)
+global mutationRate = 0.01
 
 export nLayers, maxDepth, resRange, data
 export setNLayers, setMaxDepth, setResRange, setData
@@ -52,12 +53,12 @@ end
 
 function fitness(ent::MTModel)
     global data
-    fs = data[:,1].^-1;
-    ρa, Φ = mt1d(ent.model, fs);
+    fs = data[:,1].^-1
+    ρa, Φ = mt1d(ent.model, fs)
 
-    ρRMS = rms(data[:,2], ρa, data[:,4]);
-    ΦRMS = rms(data[:,3], Φ, data[:,5]);
-    totalRMS = sqrt(ρRMS^2 + ΦRMS^2);
+    ρRMS = rms(data[:,2], ρa, data[:,4])
+    ΦRMS = rms(data[:,3], Φ, data[:,5])
+    totalRMS = sqrt(ρRMS^2 + ΦRMS^2)
 end
 
 # Scores closer to zero are better so override this.
@@ -67,6 +68,7 @@ end
 
 function group_entities(pop::Array{MTModel})
     # This'll select the best models to breed
+    # TODO: Add stopping conditions here
 end
 
 function crossover(ents::Array{MTModel})
@@ -74,7 +76,25 @@ function crossover(ents::Array{MTModel})
 end
 
 function mutate(ent::MTModel)
-    # This'll mutate a model, somehow
+    # Idea to mutate: Select a random layer to mutate the depth with,
+    # then select a random layer to mutate the resistivity with.
+    # How to mutate? -- For now, we'll just generate a new random number.
+    # TODO: Explore alternate mutations
+    global mutationRate, maxDepth, resRange;
+    if rand(Float64) > mutationRate
+        return
+    end
+
+    modelLayers = size(ent.model)[1]
+    mutatedResLayer = rand(2:modelLayers)
+    mutatedDepthLayer = rand(2:modelLayers)
+
+    mutatedRes = rand(resRange)
+    mutatedDepth = rand(1:maxDepth)
+
+    # TODO: Check we don't exceed maxDepth and resRange
+    ent.model[mutatedDepthLayer, 1] += mutatedDepth
+    ent.model[mutatedResLayer, 2] += mutatedRes
 end
 
 end
