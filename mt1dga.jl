542f9184c70f2f16d5995e405f362a2fc77be889
Alex Jackson <alex@alexj.org>
2015-11-15 21:14:07 -0700

    Add cull, mate and fill population functionsto MT1DGA

--- mt1dga.jl
+++ mt1dga.jl
@@ -2,7 +2,7 @@ include("./MT1DModel.jl")
 module MT1DGA
 using MT1DModel
 
-export MT1DModelPopulation, sortPopulation!
+export MT1DModelPopulation, sortPopulation!, matePopulation!
 
 type MT1DModelPopulation
     size::Integer
@@ -27,8 +27,32 @@ function sortPopulation!(pop::Array)
     sort!(pop, lt=(a,b) -> a.RMS < b.RMS)
 end
 
-# function advanceGeneration(MTPop::MT1DModelPopulation)
+function cullPopulation!(MTPop::MT1DModelPopulation, elitismRate::Real)
+    targetSize = floor(Integer, elitismRate * MTPop.size)
+    splice!(MTPop.pop, targetSize+1:MTPop.size)
+end
 
-# end
+function matePopulation!(MTPop::MT1DModelPopulation)
+    nParents = length(MTPop.pop)
+    for i = 1:2:nParents-1
+        child = crossover(MTPop.pop[i], MTPop.pop[i+1])
+        push!(MTPop.pop, child)
+    end
+end
+
+function fillPopulation!(MTPop::MT1DModelPopulation)
+    popToMake = MTPop.size - length(MTPop.pop)
+    for i in 1:popToMake
+        push!(MTPop.pop, createRandomModel(MTPop.layers, MTPop.zMax, MTPop.resRange, MTPop.data))
+    end
+end
+
+function advanceGeneration(MTPop::MT1DModelPopulation)
+    sortPopulation!(MT1DModelPopulation.pop)
+    cullPopulation!(MT1DModelPopulation.pop, 0.6)
+    matePopulation!(MT1DModelPopulation.pop)
+    fillPopulation!(MT1DModelPopulation.pop)
+    sortPopulation!(MT1DModelPopulation.pop)
+end
 
 end
