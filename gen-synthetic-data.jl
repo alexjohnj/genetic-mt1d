de69f6a97041c329fca48c2883d6315c99b22ab5
Alex Jackson <alex@alexj.org>
2015-11-30 19:51:51 -0700

    Add noise generation to gen-synthetic test script

--- gen-synthetic-data.jl
+++ gen-synthetic-data.jl
@@ -1,12 +1,17 @@
 include("./mt1d.jl")
 using MT1D
 
-fs = 3 * logspace(-3,3,40)
-modelFile = "./data/synthetic-4-layer.model"
+fs = logspace(-3,3,40)
+modelFile = "./data/synthetic-6-layer.model"
 outFile = replace(modelFile, ".model", ".data", 1)
 
 model = readdlm(modelFile, Float64)
 ρa, Φ = mt1d(model, fs)
 
+#Add some noise to the signal
+Φ = Φ + 0.1 * Φ .* randn(length(Φ), 1)
+ρa = ρa + 0.1 * ρa .* randn(length(ρa), 1)
+
 data = hcat(fs.^-1, ρa, Φ, ρa * 0.12, fill(3, (40,1)))
+data = round(data, 6);
 writedlm(outFile, data);
