1b1e63bf42f98fae99b2f2ff7a8a281fccbcdff6
Alex Jackson <alex@alexj.org>
2015-12-10 21:21:05 -0700

    Add example scripts and data from SNORCLE project

---
+++ examples/testGASnorcle.jl
@@ -0,0 +1,17 @@
+include("../Population.jl")
+function testGASnorcle(size=100, gen=2000)
+    inDataFile = "SNO-96-106.data"
+    outModelFile = replace(inDataFile, ".data", ".genetic.model")
+    myData = readdlm(inDataFile)
+
+    N = 4
+    zBounds = [LayerBC(0,0)
+               fill(LayerBC(1,100_000), (N-1,1))]
+    rBounds = fill(LayerBC(1,50_000), (N,1))
+
+    pop = Population(size, myData, zBounds, rBounds)
+    evolve!(pop, gen)
+
+    writedlm(outModelFile, pop.cs[1].model)
+    return pop
+end
