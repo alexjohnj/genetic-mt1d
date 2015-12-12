1230a941201fe24a2e7848143de45e0e89f3cac6
Alex Jackson <alex@alexj.org>
2015-12-11 20:44:45 -0700

    Add documentation for LayerBC type

--- src/MT1DGA.jl
+++ src/MT1DGA.jl
@@ -4,6 +4,14 @@ module MT1DGA
 using MT1D
 
 export LayerBC, Population, evolve!
+"""
+`LayerBC` defines a set of boundary conditions for a layer. One instance
+represents either the resistivity or depth boundaries.
+
+# Fields
+- `min::Integer`: Lower boundary for the layer.
+- `max::Integer`: Upper boundary for the layer.
+"""
 type LayerBC
     min::Integer
     max::Integer
